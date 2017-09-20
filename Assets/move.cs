06fc441faa066eb3c1a54ad1d83b196e1e806b10
johhhn <mkokatym@gmail.com>
2017-09-20 21:18:36 +0900

    first test

---
+++ Assets/move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+	}
+}
